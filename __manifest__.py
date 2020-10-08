6cdd1e90cf97b3eb0b05e5cb64e1eff194a12026
Tony Galmiche <tony.galmiche@infosaone.com>
2020-10-08 14:02:02 +0200

    Mise en ligne

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name'     : 'InfoSaône - Module Odoo 12 pour Fromtom',
+    'version'  : '0.1',
+    'author'   : 'InfoSaône',
+    'category' : 'InfoSaône',
+    'description': """
+InfoSaône - Module Odoo 12 pour Fromtom 
+===================================================
+""",
+    'maintainer' : 'InfoSaône',
+    'website'    : 'http://www.infosaone.com',
+    'depends'    : [
+        'base',
+    ],
+    'data' : [
+    ],
+    'installable': True,
+    'application': True,
+}
