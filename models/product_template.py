4af86fab0025c07cd046ed0746a93837e057147f
Tony Galmiche <tony.galmiche@infosaone.com>
2020-10-22 14:44:00 +0200

    bug

--- models/product_template.py
+++ models/product_template.py
@@ -5,5 +5,5 @@ from odoo import api, fields, models, _
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    is_stock_mini = fields.Integer("Stock mini")
+    is_stock_mini = fields.Float("Stock mini", digits=(14,4))
 
