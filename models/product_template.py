5aa23dbcba59b0347080b18ff33cb79500977bf2
Tony Galmiche <tony.galmiche@infosaone.com>
2021-01-08 11:50:11 +0100

    add list de prix sur fiche article

--- models/product_template.py
+++ models/product_template.py
@@ -5,7 +5,10 @@ from odoo import api, fields, models, _
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    is_stock_mini = fields.Float("Stock mini", digits=(14,4))
+    is_stock_mini         = fields.Float("Stock mini", digits=(14,4))
+    is_pricelist_item_ids = fields.One2many('product.pricelist.item', 'product_tmpl_id', 'Liste de prix')
+
+
 
 
 #    @api.multi
