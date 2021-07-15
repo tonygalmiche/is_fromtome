54c907346cd3c5b99a1008acb66e002be4843e10
Tony Galmiche <tony.galmiche@infosaone.com>
2021-07-15 16:20:45 +0200

    modifs

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -20,6 +20,19 @@ class AccountInvoice(models.Model):
     is_alerte = fields.Text('Alerte', copy=False, compute=_compute_is_alerte)
 
 
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.invoice.line'
+
+    def _compute_is_colise(self):
+        for obj in self:
+            colis=0
+            for move in obj.move_line_ids:
+                colis+=move.quantity_done
+            obj.is_colis = colis
+
+    is_colis = fields.Integer('Colis', compute=_compute_is_colise)
+
+
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
